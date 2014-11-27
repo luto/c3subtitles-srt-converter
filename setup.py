d2d4b2d43a48f2f409ae4d73f8e3c5060dd7ef20
luto <m@luto.at>
2014-11-27 15:36:45 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='c3subtitles-srt-converter',
+      version='0.1',
+      description='',
+      author='luto',
+      author_email='m@luto.at',
+      license='MIT',
+      url='https://github.com/luto/c3subtitles-srt-converter',
+      packages=['c3srtconv'],
+      install_requires=[
+      ],
+     )
